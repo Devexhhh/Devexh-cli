e0e7ae475576e1ac94f7eae51d214a0fd7d10837
Devexhhh <kumarsujal.codes@gmail.com>
2026-01-21 23:22:26 +0530

    custom colors added

---
+++ utils/colors.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+export const success = chalk.green;
+export const warning = chalk.yellow;
+export const danger = chalk.red.bold;
+export const heading = chalk.cyan.bold;
+export const muted = chalk.gray;
+export const info = chalk.cyan;
