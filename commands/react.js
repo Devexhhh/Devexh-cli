95bae0cd93f76a549c92204d2f42cf69e9bc53e4
Devexhhh <kumarsujal.codes@gmail.com>
2026-04-30 22:21:40 +0530

    fixed indentation

--- commands/react.js
+++ commands/react.js
@@ -10,20 +10,16 @@ export default function react(program) {
         .description("Create a React project using Vite")
         .action((name, options) => {
             let template = "react";
-
             if (options.ts) template = "react-ts";
             if (options.js) template = "react";
-
             console.log(
                 chalk.cyan(`Creating React project '${name}' using ${template}...`)
             );
-
             try {
                 execSync(
                     `npm create vite@latest ${name} -- --template ${template}`,
                     { stdio: "inherit" }
                 );
-
                 console.log(chalk.green("\nProject created successfully!\n"));
                 console.log("Next steps:");
                 console.log(`  cd ${name}`);
